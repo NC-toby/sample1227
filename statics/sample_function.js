e91251ff0cee563d5b02cc3cf4b93e394f3148be
TOBY <info@sample.com>
2023-11-14 11:08:31 +0000

    add function

---
+++ statics/sample_function.js
@@ -0,0 +1,20 @@
+console.log("Hello");
+
+let userName = "john";
+
+function showMessage(from,text) {
+
+    console.log(from);
+    console.log(text);
+}
+
+
+function sayHi(){
+    let message = "Hi, I'm"
+    console.log(message,userName);
+}
+
+showMessage("Ann");
+
+sayHi();
+
